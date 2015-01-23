15d71d9ab2c5cb5f03eb1143c73a8faf0248dabe
plotnikovanton <plotnikovanton@gmail.com>
2015-01-23 23:30:45 +0300

    configurabe wireless widget

--- wireless.lua
+++ wireless.lua
@@ -12,13 +12,14 @@ local function worker(args)
 
     -- Settings
     local ICON_DIR  = awful.util.getdir("config").."/net_widgets/icons/"
-    local INTERFACE = "wlp1s0"
+    local interface = args.interface or "wlan0"
+    local timeout   = args.timeout or 5
         
     local net_icon = wibox.widget.imagebox()
     net_icon:set_image(ICON_DIR.."wireless_na.png")
     local net_text = wibox.widget.textbox()
     net_text:set_text(" N/A ")
-    local net_timer = timer({ timeout = 5 })
+    local net_timer = timer({ timeout = timeout })
     local function net_update() 
         local signal_level = tonumber(awful.util.pread("awk 'NR==3 {printf \"%3.0f\" ,($3/70)*100}' /proc/net/wireless"))
         if signal_level == nil then
@@ -50,28 +51,27 @@ local function worker(args)
     local function text_grabber()
         local msg = ""
         if connected then
-            f = io.popen("iwconfig "..INTERFACE)
+            f = io.popen("iwconfig "..interface)
            
-            line    = f:read()      -- wlp1s0    IEEE 802.11abgn  ESSID:"ESSID" 
+            line    = f:read() or ""    -- wlp1s0    IEEE 802.11abgn  ESSID:"ESSID" 
             essid   = string.match(line, "ESSID:\"(.+)\"") or " N/A "
-            line    = f:read()      -- Mode:Managed  Frequency:2.437 GHz  Access Point: aa:bb:cc:dd:ee:ff
+            line    = f:read() or ""    -- Mode:Managed  Frequency:2.437 GHz  Access Point: aa:bb:cc:dd:ee:ff
             mac     = string.match(line, "Access Point: (.+)") or " N/A "
-            line    = f:read()      -- Bit Rate=36 Mb/s   Tx-Power=15 dBm 
+            line    = f:read() or ""    -- Bit Rate=36 Mb/s   Tx-Power=15 dBm 
             bitrate = string.match(line, "Bit Rate=(.+/s)") or " N/A "
-            line    = f:read()      -- 
     
             f:close()
-            f = io.popen("ifconfig "..INTERFACE)
+            f = io.popen("ifconfig "..interface)
             
-            line    = f:read()      -- wlp1s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
-            line    = f:read()      -- inet 192.168.1.15  netmask 255.255.255.0  broadcast 192.168.1.255
+            line    = f:read() or ""    -- wlp1s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+            line    = f:read() or ""    -- inet 192.168.1.15  netmask 255.255.255.0  broadcast 192.168.1.255
             inet    = string.match(line, "inet (%d+%.%d+%.%d+%.%d+)") or " N/A "
     
             f:close()
             
     
             msg = 
-                "┌["..INTERFACE.."]\n"..
+                "┌["..interface.."]\n"..
                 "├ESSID:\t\t"..essid.."\n"..
                 "├IP:\t\t"..inet.."\n"..
                 "├BSSID\t\t"..mac.."\n"..
